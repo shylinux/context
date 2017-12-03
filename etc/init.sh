e2328f574672a1cd41c5361e8248e4827562ae18
shaoying <shylinux@icloud.com>
2017-12-03 22:07:21 +0800

    mac pro ctx 整理代码

--- etc/init.sh
+++ etc/init.sh
@@ -1,34 +1,13 @@
 # @debug on
-# ~root aaa
-# login root root
-#
-
 source etc/lex.sh
 
 ~root mdb
 open chat chat "chat:chat@/chat" mysql
 
 ~root aaa
+login shy shy
 login root root
 
-# ~root
-# $nserver
-
-#
-# login root 94ca7394d007fa189cc4be0a2625d716 root
-
-# ~root tcp
-# listen ":9393"
-# listen ":9394"
 ~root cli
 remote slaver listen ":9393" tcp
-
-# ~aaa
-# login shy shy
-# userinfo add context hi hello nice
-# userinfo add command hi context
-# ~web
-# listen
-# ~demo
-# listen
-# ~home spawn test
+~root aaa
